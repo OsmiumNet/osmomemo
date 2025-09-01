a97472e860a8c137a6fa82b616c5d3463e786521
JustNevi <slavkad25@gmail.com>
2025-09-01 14:33:29 +0200

    test: update keys tests

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -1,5 +1,8 @@
 import unittest
 
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey, X25519PublicKey
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey  
+
 from osmomemo import XKeyPair
 from osmomemo import EdKeyPair
 
@@ -11,3 +14,18 @@ class TestKeys(unittest.TestCase):
         self.assertIsInstance(xpair, XKeyPair)
         self.assertIsInstance(edpair, EdKeyPair)
 
+        self.assertIsInstance(xpair.get_private_key(), X25519PrivateKey)
+        self.assertIsInstance(xpair.get_public_key(), X25519PublicKey)
+        self.assertIsInstance(edpair.get_private_key(), Ed25519PrivateKey)
+        self.assertIsInstance(edpair.get_public_key(), Ed25519PublicKey)
+
+    def test_converting(self):
+        edpair = EdKeyPair.generate()
+
+        self.assertIsInstance(edpair.get_x_private_key(), X25519PrivateKey)
+        self.assertIsInstance(edpair.get_x_public_key(), X25519PublicKey)
+
+
+        xpair = XKeyPair(edpair.get_x_private_key())
+
+        self.assertEqual(xpair.get_public_key(), edpair.get_x_public_key())
