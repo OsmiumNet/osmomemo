53b957d67a5fd170236d3af64668b845089b9749
JustNevi <slavkad25@gmail.com>
2025-09-09 13:41:52 +0200

    build: update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="osmomemo",
-    version="1.0.0",
+    version="1.1.0",
 
     author="osmiumnet",
     description="Python omemo package",
